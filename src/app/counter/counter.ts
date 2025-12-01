a20f95a4001af8cb7a60cd58423bea280019617f
taaher-k <taaher.rehmanii@gmail.com>
2025-12-02 00:10:54 +0530

    counter

--- src/app/counter/counter.ts
+++ src/app/counter/counter.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-counter',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './counter.html',
   styleUrls: ['./counter.css'],
 })
 export class Counter {
+  public currentCount = 0;
+  public isLimitReached = false;
 
+  public increment() {
+    this.currentCount++;
+    if (this.currentCount >= 10) {
+      this.isLimitReached = true;
+    }
+  }
 }
