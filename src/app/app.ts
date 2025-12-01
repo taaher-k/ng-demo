d3e7ccf649cee4dc60045d2354f33c467df15410
taaher-k <taaher.rehmanii@gmail.com>
2025-12-02 00:00:40 +0530

    counter

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,19 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Counter } from './counter/counter';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [Counter],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
+
+
+
+
+
 export class App {
   protected readonly title = signal('ngdemo');
 }
---
+++ src/app/counter/counter.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  standalone: true,
+  imports: [],
+  templateUrl: './counter.html',
+  styleUrls: ['./counter.css'],
+})
+export class Counter {
+
+}
